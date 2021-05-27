cd1c8cd7ee8fbdf8d1e3ab006ecc37ee43b9d134
Franceeesc00 <71765883+Franceeesc00@users.noreply.github.com>
2021-05-27 11:35:02 +0200

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,13 @@
+   window.onscroll = function() {
+       scrollFunction()
+   };
+
+   function scrollFunction() {
+       if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+           document.getElementById("nav").style.backgroundColor = "rgba(255,255,255,0.0)";
+           document.getElementById("nav").style.backdropFilter = "blur(4px)";
+       } else {
+           document.getElementById("nav").style.backgroundColor = "rgba(0,0,0,0.0)";
+           document.getElementById("nav").style.backdropFilter = "blur(4px)";
+       }
+   }
